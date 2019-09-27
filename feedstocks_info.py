bf67a5d68ad5258c150bc6cb71b9b055f9dd05de
Maksim Rakitin <mrakitin@bnl.gov>
2019-09-27 19:44:41 -0400

    Add a version check

--- feedstocks_info.py
+++ feedstocks_info.py
@@ -1,12 +1,14 @@
 import glob
+import os
 
 import git
+import markdown
 import pandas as pd
+import requests
+from bs4 import BeautifulSoup
 from tabulate import tabulate
 
 all_feedstocks = sorted(glob.glob("*-feedstock"))
-
-columns = ['Name', 'Branch', 'Changed?']
 
 # header = f'{"#":>5s} | {"Name":^40s} | {"Branch":^20s} | {"Changed?":^10s} |'
 # separator = "-" * len(header)
@@ -15,11 +17,25 @@ columns = ['Name', 'Branch', 'Changed?']
 
 info = []
 for i, feedstock in enumerate(all_feedstocks):
+
+    # Get version info from README.md's badge via requesting the info from svg:
+    try:
+        html_text = markdown.markdown(open(os.path.join(feedstock, 'README.md')).read())
+        html = BeautifulSoup(html_text, features='lxml')
+        svg = html.findAll('img', attrs={'alt': 'Conda Version'})[0]
+        r = requests.get(svg.attrs['src'])
+        svg_html = BeautifulSoup(r.text, features='lxml')
+        version_tag = svg_html.findAll('text')[-1]
+        version = version_tag.text
+    except:
+        version = ''
+
+    # Extract info from git:
     repo = git.Repo(feedstock)
-    info.append([feedstock, repo.active_branch.name, repo.is_dirty()])
+    info.append([feedstock, repo.active_branch.name, repo.is_dirty(), version])
     # print(f"{i:>5d} | {feedstock:40s} | {repo.active_branch.name:^20s} | {str(repo.is_dirty()):^10s} |")  # noqa
 
 # print(separator)
-
+columns = ['Name', 'Branch', 'Changed?', 'Version']
 df = pd.DataFrame(info, columns=columns)
 print(tabulate(df, headers=df.columns))
