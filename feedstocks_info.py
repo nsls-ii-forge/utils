557cb9e900e15797d28d3e1019d6c486f620f3f6
Maksim Rakitin <mrakitin@bnl.gov>
2019-09-27 18:33:49 -0400

    Use tabulate and pandas dataframe

--- feedstocks_info.py
+++ feedstocks_info.py
@@ -1,19 +1,25 @@
 import glob
-import pandas as pd
-import git
 
-df = pd.DataFrame()
+import git
+import pandas as pd
+from tabulate import tabulate
 
 all_feedstocks = sorted(glob.glob("*-feedstock"))
 
-header = f'{"#":>5s} | {"Name":^40s} | {"Branch":^20s} | {"Changed?":^10s} |'
-separator = "-" * len(header)
+columns = ['Name', 'Branch', 'Changed?']
 
-print(f"\nTotal number of feedstocks: {len(all_feedstocks)}\n")
-print(f"{separator}\n{header}\n{separator}")
+# header = f'{"#":>5s} | {"Name":^40s} | {"Branch":^20s} | {"Changed?":^10s} |'
+# separator = "-" * len(header)
+# print(f"\nTotal number of feedstocks: {len(all_feedstocks)}\n")
+# print(f"{separator}\n{header}\n{separator}")
 
+info = []
 for i, feedstock in enumerate(all_feedstocks):
     repo = git.Repo(feedstock)
-    print(f"{i:>5d} | {feedstock:40s} | {repo.active_branch.name:^20s} | {str(repo.is_dirty()):^10s} |")  # noqa
+    info.append([feedstock, repo.active_branch.name, repo.is_dirty()])
+    # print(f"{i:>5d} | {feedstock:40s} | {repo.active_branch.name:^20s} | {str(repo.is_dirty()):^10s} |")  # noqa
 
-print(separator)
+# print(separator)
+
+df = pd.DataFrame(info, columns=columns)
+print(tabulate(df, headers=df.columns))
